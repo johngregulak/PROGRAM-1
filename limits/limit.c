d4d8bc9cd6e1599d51af63aafe50bb465d8f093a
johngregulak <johngregulak@gmail.com>
2015-09-14 13:37:08 -0400

    Working limits.c

--- limits/limit.c
+++ limits/limit.c
@@ -5,13 +5,16 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include <string.h>
 
 
 int main(){
 
     int childID, status;
     pid_t childPID = fork();
+    char file[256] = "/proc/";
+    char PID[256];
+    char location[256] = "/limits";
 
     if (childPID == 0)  // child process
     {
@@ -21,17 +24,31 @@ int main(){
     else if (childPID > 0)  // parent process
     {
         //wait for child process to finish
-        childID = getpid();
 
+        childID = getpid();
+        sprintf(PID, "%d", childID);
+        printf("This is PID, childID: %s, %d\n", PID, childID);
+        strcat(file, PID);
+        strcat(file, location);
+        //printf("Info is in: %s \n", file);
+        FILE * fp;
+        char limit_string[256];
+        fp = fopen (file, "r");
+
+        while (fgets(limit_string, sizeof(limit_string), fp)) {
+        printf("%s", limit_string);
+        }
 
         printf("Parent process has completed. Child ID is %d\n", childID);
 		waitpid(childPID, &status, 0);
+		fclose(fp);
     }
     else  // fork failed
     {
         printf("Fork() has failed!\n");
         return 1;
     }
+    /*
     printf("|=====================================================================|\n");
     printf("|Printing Limits for Command:                                         |\n");
     printf("|=====================================================================|\n");
@@ -41,7 +58,7 @@ int main(){
     printf("|Max Pending Signals:                                      Signals    |\n");
     printf("\n");
 
-
+    */
     return 0;
 
 }
